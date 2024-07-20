5edca102a804f3f3fd69b4429d102b65f787bd07
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-07-19 23:00:49 -0300

    prisma fix bugs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,11 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService {
+  public client: PrismaClient;
 
-  async onModuleDestroy() {
-    await this.$disconnect();
+  constructor() {
+    this.client = new PrismaClient();
   }
 }
