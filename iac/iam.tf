e6611bf006f3417520ed4bcf2e72e66000ff6cd1
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-12 17:49:22 -0300

    ajustes pipeline ecr

--- iac/iam.tf
+++ iac/iam.tf
@@ -12,6 +12,33 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "app-runner-role" {
+  name = "app-runner-role"
+
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Sid : "Statement1",
+        Effect : "Allow",
+        Principal : {
+          Service : "build.apprunner.amazonaws.com"
+        },
+        Action : "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ]
+
+  tags = {
+    IAC = "True"
+  }
+
+}
+
 resource "aws_iam_role" "ecr_role" {
   name = "ecr_role"
 
@@ -41,33 +68,38 @@ resource "aws_iam_role" "ecr_role" {
   })
 
   inline_policy {
-    name = "ecr-app-permission"
-
+    name = "ecr-app-permissions"
     policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
+      Statement = [{
+        Sid      = "Statement1"
+        Action   = "apprunner:*"
+        Effect   = "Allow"
+        Resource = "*"
+        },
         {
+          Sid = "Statement2"
           Action = [
-            "ecr:GetAuthorizationToken",
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:GetRepositoryPolicy",
-            "ecr:DescribeRepositories",
-            "ecr:ListImages",
-            "ecr:DescribeImages",
-            "ecr:BatchGetImage",
-            "ecr:GetLifecyclePolicy",
-            "ecr:GetLifecyclePolicyPreview",
-            "ecr:ListTagsForResource",
-            "ecr:DescribeImageScanFindings",
-            "ecr:InitiateLayerUpload",
-            "ecr:UploadLayerPart",
-            "ecr:CompleteLayerUpload",
-            "ecr:PutImage"
+            "iam:PassRole",
+            "iam:CreateServiceLinkedRole",
           ]
           Effect   = "Allow"
           Resource = "*"
         },
+        {
+          Sid = "Statement3"
+          Action = [
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:PutImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:GetAuthorizationToken",
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        }
       ]
     })
   }
