369333d17b834e630aaf882e79eb266a8a64c169
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-09-13 10:40:42 -0300

    update: hello world

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Deploy Pipeline GitHub Actions (CI CD), AWS Registry, AWS App Runner'
+    return 'Hello World!'
   }
 }
